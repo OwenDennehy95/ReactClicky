5963170aeb6a31ee8dd68a72370c3f7fc3c695e8
finnias <owen.dennehy@me.com>
2019-07-23 15:14:56 -0400

    cssupdate

--- react-clicky-app/src/components/Nav/Nav.js
+++ react-clicky-app/src/components/Nav/Nav.js
@@ -4,7 +4,7 @@ import './Nav.css';
 const Nav = props => (
 <nav className="navbar navbar-dark bg-danger">
     <h1>Clicky Game</h1>
-    <span className='navbar-text'>Click images only once to win!</span>
+    <span className='navbar-text'>Click the instruments only once to win!</span>
     <ul className='nav nav-fills'>
         <li className='nav-item'>
             <span className='nav-link'>Score: {props.score} | Top Score: {props.topScore}</span>
